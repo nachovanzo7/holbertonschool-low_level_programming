c9acf47334d4bd579a63dbf994b8a77ee7eea150
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-13 20:39:51 +0000

    .

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
-	printf("is zero");
+	printf("is zero\n");
 	else if (n > 0)
-	printf("is positive");
+	printf("is positive\n");
 	else
-	printf("is negative");
+	printf("is negative\n");
 	return (0);
 }
