958b09f7c35b2cf1adf2101f1a48d9116eb71487
root <9426@holbertonstudents.com>
2024-06-14 14:31:49 +0000

    .

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,14 +8,15 @@
 int main(void)
 {
 	char x;
+	char y;
 
 	for (x = 0; x <= 10; x++)
 	{
 		putchar(x);
 	}
 
-	for (x = 'a'; x <= 'f'; x++)
-		putchar(x);
+	for (y = 'a'; y <= 'f'; y++)
+		putchar(y);
 
 	putchar('\n');
 	return (0);
