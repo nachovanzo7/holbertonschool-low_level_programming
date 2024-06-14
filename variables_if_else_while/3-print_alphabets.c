aa822b21a1163cc44b5d60478565e8a283fb6452
root <9426@holbertonstudents.com>
2024-06-14 12:47:20 +0000

    .

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - Devuelve el abecedario 3 veces
+*Return: Devuelve siempre 0
+*/
+
+int main(void)
+{
+	char min;
+
+	for (min = 'a'; min <= 'z'; min++)
+		putchar(min);
+
+	for (min = 'a'; min <= 'z'; min++)
+		putchar(toupper(min));
+}
