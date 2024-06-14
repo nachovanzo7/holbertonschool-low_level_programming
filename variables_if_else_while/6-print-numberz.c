d75a37eb35a7abaa3052c3043054ece68fda6904
root <9426@holbertonstudents.com>
2024-06-14 13:50:29 +0000

    .

---
+++ variables_if_else_while/6-print-numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Devolver todos los numeros de base 10
+*Return: Retorna siempre 0
+*/
+
+int main(void)
+{
+	char num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+
+	return (0);
+}
