61ecc366f6a4084ee7ebdd76f660d708fb5f224d
root <9426@holbertonstudents.com>
2024-06-14 16:46:19 +0000

    .

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Imprimir _putchar
+*Return: Retorna siempre 0
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Vamo que vamoo
