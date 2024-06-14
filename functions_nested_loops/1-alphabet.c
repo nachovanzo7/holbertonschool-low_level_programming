fd462c25caccdfbc98ea70e11c90680ba0b97a8c
root <9426@holbertonstudents.com>
2024-06-14 17:56:52 +0000

    ErrorFunciones

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_alphabet - Devuelva el abecedario en minusculas
+*Return: No devuelve nada
+*/
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+}
---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "1-alphabet.c"
+/**
+*main - Main del ejercicio 1
+*Return: Siempre es 0
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
