dde6e78aaa8c12bc951520063d64eda53468a02b
root <9426@holbertonstudents.com>
2024-06-14 13:42:06 +0000

    .

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	int num = 0;
-	while (num % 10 == 0 || num == 0)
+	int num;
+
+	for (num = 0; num < 10; num++)
 	{
 		printf("%i", num);
-		num++;
 	}
 	printf("\n");
 	return (0);
