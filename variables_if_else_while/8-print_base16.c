bfae46c94b7703421157fce555b2c4d5ba3afcbb
root <9426@holbertonstudents.com>
2024-06-14 14:27:42 +0000

    .

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - Retorna todos los numeros de base 16
+*Return: devuelve siempre valor 0
+*/
+
+int main(void)
+{
+	char x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		putchar(x);
+	}
+
+	for (x = a; x <= f; x++)
+		putchar(x);
+
+	putchar('\n');
+	return (0);
+}
