c3f9ec699c42a3e89aaea026deb269bc24aff0e5
root <9426@holbertonstudents.com>
2024-06-14 17:00:25 +0000

    .

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "_putchar.c"
 
 /**
 *main - Imprimir _putchar
@@ -7,13 +8,13 @@
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
