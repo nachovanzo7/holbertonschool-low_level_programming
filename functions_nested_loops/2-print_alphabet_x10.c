77a49cc8bebb2ba132e9d8d6fa85bc5a0f969daf
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-17 12:02:45 +0000

    ImprimirAbecedariox10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_alphabet_x10 - Va a imprimir el abecedario diez veces
+*Return: no devuelve nada
+*/
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i <= 10; i++)
+	{
+		char letra;
+
+		for (letra = 'a'; letra <= 'z'; letra++)
+			putchar(letra);
+
+		putchar('\n');
+	}
+}
