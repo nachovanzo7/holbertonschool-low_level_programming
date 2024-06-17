e5b7b7ec6124d65222a43890c6dcd394035d6aff
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-17 14:17:51 +0000

    Tablasx10

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*times_table - Tablas del 1 al 10
+*Return: sin retorno
+*/
+
+void times_table(void)
+{
+
+	int vueltas;
+	int num;
+
+	for (vueltas = 0; vueltas <= 10; vueltas++)
+	{
+		for (num  = 0; num == 10; num++)
+		{
+			_putchar(vueltas * num);
+
+			if (num != 10)
+{
+				_putchar(',');
+				_putchar(' ');
+}
+		}
+
+	}
+}
