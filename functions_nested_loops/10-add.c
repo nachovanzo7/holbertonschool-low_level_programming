614479a4d1236f43b707fa99a545971ea42fe428
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-17 16:45:11 +0000

    SumaDeNumeros

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*add - Sumar dos numeros y devolver el resultado
+*Return: devuelve el valor de la suma (entero)
+*@x: numero uno
+*@y: numero dos
+*/
+
+int add(int x, int y)
+{
+	int suma;
+
+	suma = x + y;
+	return (suma);
+}
