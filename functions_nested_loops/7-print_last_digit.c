983dbb46d27ce9dd8ac7a233136d8882ea09f7bd
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-17 13:39:00 +0000

    UltimoDigito

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_last_digit - Devuelve el ultimo digito de un numero
+*Return: ultimo digito de un numero (int)
+*@x: numero ingresado
+*/
+
+int print_last_digit(int x)
+{
+	int last = x % 10;
+
+	return (last);
+}
