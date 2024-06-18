80d258c625bacd377ed627ff21b7684a424c3b2a
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-18 14:14:12 +0000

    LineaTerminal

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_line - Dibuja una linea en la terminal
+*Return: no devuelve nada
+*@n: cantidad de veces que se repite la linea
+*/
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	else
+	{
+		for (x = 0; x <= n; x++)
+			_putchar('_');
+
+		_putchar('\n');
+	}
+}
