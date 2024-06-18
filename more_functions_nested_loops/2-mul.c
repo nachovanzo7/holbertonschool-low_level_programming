b6af4d5bc7aea1c7f96188d83e4e83fa14cd534d
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-18 13:22:48 +0000

    MultiplicarValores

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*mul - Multiplica dos numeros
+*Return: retorna el valor
+*@a: valor numero 1
+*@b: valor numero 2
+*/
+
+int mul(int a, int b)
+{
+	int x;
+
+	x = (a * b);
+
+	return (x);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _putchar(char x);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
