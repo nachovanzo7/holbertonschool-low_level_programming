972c2ddba2d20da55ad5984ae3234300c87ffd8b
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-18 13:36:43 +0000

    Del9al0Reducido

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_most_numbers - Retornar numero del 0 al 9 sin incluir al 2 y 4
+*Return: no retorna nada
+*/
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x != 2 && x != 4)
+			_putchar(x);
+	}
+
+	_putchar('\n');
+}
