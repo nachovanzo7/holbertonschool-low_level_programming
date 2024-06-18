419c4b2ac1ef3359981d38b81b5d4888ab69024c
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-18 13:26:32 +0000

    Numeros0a9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_numbers - Devuelve los numeros entre 0 a 9
+*Return: no retorna nada
+*/
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		_putchar(x);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,5 +5,6 @@ int _putchar(char x);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
