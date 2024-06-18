a7a22b2e6d3a383fe16b8a597aacbbf5414903a4
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-18 14:00:10 +0000

    NumerosBucle

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,14 +4,24 @@ void more_numbers(void)
 {
 	int x;
 	int y;
+	int decena;
+	int unidad;
 
-	for (x = 1; x <= 10; x++)
+	for (x = 0; x < 9; x++)
 	{
 		for (y = 0; y <= 14; y++)
-		_putchar(y);
+		{
+			unidad = y % 10;
+			decena = y / 10;
+
+			if (y == 9)
+			{
+				_putchar(decena + '0');
+			}
+
+			_putchar(unidad + '0');
+		}
 
 		_putchar('\n');
 	}
-
-
 }
