74a7975a009a32f966510a0c2f25f27618c46728
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-18 17:34:50 +0000

    FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x < 99; x++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (((x % 3) == 0) && ((x % 5) == 0))
+		if ((x % 3 == 0) && (x % 5 == 0))
 			printf("FizzBuzz");
 		else if ((x % 5) == 0)
 			printf("Buzz");
