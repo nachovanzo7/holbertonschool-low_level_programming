0b6c82c53a93825ee187f9c950608b324bd6276f
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-20 14:01:44 +0000

    Cantidad

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_strlen - Contar el largo del string
+*Return: no retorna nada
+*@s: cada letra del string
+*/
+
+int _strlen(char *s)
+{
+	int cant;
+	char x;
+
+	cant = 0;
+
+	for (x = *s; x == '\0'; x++)
+	{
+
+		cant++;
+	}
+
+	return (cant);
+}
+
