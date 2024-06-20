b96444ad52d9eb1f64aa3aefc0a4338405ef41c0
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-20 15:54:14 +0000

    Reversa

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,10 @@ void print_rev(char *s)
 	int x = 0;
 	int y;
 
-	while(s[x] != '\0')
+	while (s[x] != '\0')
 		x++;
 
-	for(y = (x - 1); y >= 0; y--)
+	for (y = (x - 1); y >= 0; y--)
 	{
 		_putchar(s[y]);
 	}
