b9b8f29a77df9ca8a538bf857773ba769ecca47b
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-20 13:03:12 +0000

    Punteros

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*reset_to_98 - Cambiar el valor de funcio main
+*Return: no se retorna nada
+*@n: la direccion de la variable que vamos a cambiar
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
