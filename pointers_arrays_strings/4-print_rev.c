6cc131eb7fbe2f6d4e21fac06ba28a4b1d57b015
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-20 15:53:16 +0000

    Reversa

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_rev - Devuelve una string en reversa
+*Return: no retorna nada
+*@s: el string devuelto
+*/
+
+void print_rev(char *s)
+{
+	int x = 0;
+	int y;
+
+	while(s[x] != '\0')
+		x++;
+
+	for(y = (x - 1); y >= 0; y--)
+	{
+		_putchar(s[y]);
+	}
+
+	_putchar('\n');
+}
+
+
