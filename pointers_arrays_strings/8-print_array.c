095324ccacde5e80c7fe746d8723960ff44f1373
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-21 14:26:28 +0000

    Arrays

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,13 @@ void print_array(int *a, int n)
 	int x;
 
 	for (x = 0; x <= n; x++)
+	{
 		printf("%d", a[x]);
 
-	if (x != (n - 1))
-	{
-		printf(", ");
+		if (x != (n - 1))
+		{
+			printf(", ");
+		}
 	}
 
 	printf("\n");
