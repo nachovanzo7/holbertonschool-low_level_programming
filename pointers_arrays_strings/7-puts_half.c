b21105fb040b9575f1d69a57676fb1d551df901b
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-21 12:49:12 +0000

    MitadString

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,23 +9,23 @@
 void puts_half(char *str)
 {
 	int x = 0;
-	int y;
+	int n;
 
 	while (str[x] != '\0')
 		x++;
 
 	if (x % 2 == 1)
 	{
-		y = ((x - 1) / 2);
-		y += 1;
+		n = (x - 1) / 2;
+		n += 1;
 	}
 
 	else
-		y = x / 2;
+		n = x / 2;
 
-	for (; y < x; y++)
+	for (; n < x; n++)
 	{
-		_putchar(str[y]);
+		_putchar(str[n]);
 	}
 
 }
