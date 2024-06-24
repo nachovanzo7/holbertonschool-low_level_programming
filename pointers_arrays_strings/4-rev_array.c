ebf6694c7abf165df9ceff11dda019a748d0b2c3
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-24 17:34:55 +0000

    Invertir un Array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*reverse_array - Devuelve array de enteros invertido
+*Return: no retorna nada
+*@a: array de enteros
+*@n: cantidad de espacios en array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int y = 0;
+	int x;
+
+	if (n % 2)
+	{
+		for (x = n; x >= 0; x--)
+		{
+			if (x != y)
+				a[x] = a[y];
+
+			y++;
+		}
+	}
+}
