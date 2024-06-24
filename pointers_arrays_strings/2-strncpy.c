feb1b7aff266428a86f59954134dd46ff53e34a5
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-24 14:41:48 +0000

    Copiar string en otro

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_strncpy - Copiar un string limitado
+*Return: retorna el string copiado en el otro
+*@dest: el string modificado y devuelto
+*@src: string que se le copia
+*@n: cantidad de carateres que se le copia al string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+/*  A partir del string inicial le copio el otro string en el comienzo */
+
+	for (x = 0; x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	return (dest);
+}
+
