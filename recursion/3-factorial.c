86f7375c7edaf721654548d599882432ff43a499
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-26 17:38:36 +0000

    Factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *factorial - Devuelve el factorial de un numero
+ *Return: si es menor a 0 se retorna -1, caso contrario el factorial
+ *@n: el numero para calcular al factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return -1; /*Indica error*/
+	else if (n == 0)
+		return 1; /*Factorial de 0 es 1*/
+
+	else
+	{
+		
+	}
+
+}
