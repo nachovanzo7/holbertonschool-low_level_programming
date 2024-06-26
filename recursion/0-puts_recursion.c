aeb176aeff128987b37eec100558a743fcccc1da
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-26 14:01:18 +0000

    Imprimir string con recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *_puts_recursion - Imprimir un string 
+ *_puts_recursion - Imprimir un string
  *Return: no retorna nada
  *@s: el string para imprimir
  */
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 {
 
 	if (*s == '\0')
-	{	
+	{
 		_putchar('\n');
 		return;
 	}
