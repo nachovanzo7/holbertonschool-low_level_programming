e83a632708d3c58d20d366ebbe706c149d23ef53
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-26 17:25:07 +0000

    Longitud de string

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_strlen_recursion - Devuelve la longitud del string
- *Return: retorna el largo del string
- *@s: string para medir su longitud
- */
+*_strlen_recursion - Devuelve la longitud del string
+*Return: retorna el largo del string
+*@s: string para medir su longitud
+*/
 
 int _strlen_recursion(char *s)
 {
@@ -13,7 +13,7 @@ int _strlen_recursion(char *s)
 	if (*s) /*recorro el string*/
 	{
 		x++; /*Cada vuelta se le suma uno a la variable longitud*/
-		x += _strlen_recursion(s + 1); 
+		x += _strlen_recursion(s + 1);
 		/*Continua en la siguiente posicion del string (s + 1)*/
 	}
 
