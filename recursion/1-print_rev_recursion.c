8cbdafb86d25271d4b4dbd9201dba6a53af50f14
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-26 14:39:43 +0000

    Imprimir string reverso

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - Imprime el string en reversa
+ *Return: no retorna nada
+ *@s: string para imprimir en reversa
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s) /*recorro el string hasta el final*/
+	{
+		_print_rev_recursion(s + 1); /*voy hasta el final del string de uno en uno*/
+		_putchar(*s); /*Se va a ejecutar al llegar al ultimo caracter y 
+				comienza a imprimir del final al principio*/
+	}
+	
+	return;
+}
