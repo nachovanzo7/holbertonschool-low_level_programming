1cb84e092775babf997dc443c60a0d582913c3f2
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-27 13:02:49 +0000

    Factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -10,12 +10,16 @@ int factorial(int n)
 {
 	if (n < 0)
 		return -1; /*Indica error*/
-	else if (n == 0)
+	else if (n == 0 || n == 1)
 		return 1; /*Factorial de 0 es 1*/
 
-	else
-	{
-		
-	}
+	/* Factorial de X: !5 = 5 * 4 * 3 .. * 1 */
+	/* Se calcula de forma asc - (1 * 2) * (2 * 3) * (6 * 4) * (24 * 5) */
+
+	n *= factorial(n - 1);
+	/* (*=) al llegar a 1 se retorna 1 y comienza la ecuacion anterior */
+
+	return (n);
+
 
 }
