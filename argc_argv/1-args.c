c667ca7967cee77177ab9ff2a72fd02df471dfd1
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-28 17:57:01 +0000

    Cantidad de elementos

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Retorna cantidad de argumentos
+ *Return: retorna 0
+ *@argc: argumentos pasados 
+ *@argv: array
+ */
+
+int main(int argc, char *argv[])
+{	
+	printf("%d\n", len);
+	
+	return (0);
+}
