b30465b563f7cc7f6aca48396d22d91902af0216
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-29 20:49:48 +0000

    Args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,13 +3,12 @@
 /**
  *main - Retorna cantidad de argumentos
  *Return: retorna 0
- *@argc: argumentos pasados 
+ *@argc: argumentos pasados
  *@argv: array
  */
 
 int main(int argc, char *argv[])
-{	
-	printf("%d\n", len);
-	
+{
+	printf("%d\n", argc - 1); /* El ultimo argumento es NULL */
 	return (0);
 }
