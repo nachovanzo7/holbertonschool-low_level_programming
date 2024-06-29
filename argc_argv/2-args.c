57c4a036d3f2859483e34269ec17595f7d0e6309
Nacho Vanzo <9426@holbertonstudents.com>
2024-06-29 21:03:46 +0000

    Imprimir Argumentos

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Imprime todos los argumentos recibidos
+ *Return: retorna 0
+ *@argc: cantidad de argumentos
+ *@argv: array
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 1; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}	
+	
+	return (0);
+
+}
