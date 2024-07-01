820c8aa711df478224470e698e2ee6e06809c95c
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-01 18:02:26 +0000

    Limpiar memoria

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -10,15 +10,14 @@
 
 void free_grid(int **grid, int height)
 {
-	int x; 
+	int x;
 	int **p = grid;
 
 	for (x = 0; x < height; x++) /* Recorro cada una de las filas */
 	{
+		for (x = 0; p[x] != NULL; x++) /*Limpio uno a uno los espacios creados */
+			free(p[x]);
 
-			for(x = 0; p[x] != NULL; x++) /*Limpio uno a uno los espacios creados */
-				free(p[x]);
-
-			free(p); /* Limpio todo el espacio */
+		free(p); /* Limpio todo el espacio */
 	}
 }
