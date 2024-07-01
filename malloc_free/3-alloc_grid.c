01ac1f507b49b4dcffc85bae018fb2c25754c7e4
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-01 17:16:34 +0000

    Array bidimensional

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,13 +17,15 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = malloc((width * height) * sizeof(int));
+	p = malloc(height * sizeof(int));
 
 	if (p == NULL)
 		return (NULL);
 
 	for (x = 0; x <= height; x++)
 	{
+		p[x] = malloc(width * sizeof(int));
+
 		for (y = 0; y <= width; y++)
 		{
 			p[y][x] = 0;
