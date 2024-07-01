feb3f2f5ce6502f1f3797afc2916613c0b93a512
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-01 16:25:03 +0000

    Concatenar Strings

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *str_concat - Concatena dos string
+ *Return: retorna el string o NULL en caso de error
+ *@s1: primer string
+ *@s2: segundo string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len = 0;
+	int x;
+	char *p;
+	int y = 0;
+
+	if (s1 == NULL || s2 == NULL) /*El string ingresado no es nulo */
+		return (NULL);
+
+	for (x = 0; s1[x]; x++)
+	      len++;
+
+	for (x = 0; s2[x]; x++)
+		len++;
+
+	p = malloc(sizeof(char) * (len + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+		p[x] = s1[x];
+		y++;
+	}
+
+	for (x = 0; s2[x] != '\0'; x++)
+	{
+		p[y] = s2[x];
+		y++;
+	}
+
+	p[y] = '\0';
+
+	return (p);
+
+
+}
