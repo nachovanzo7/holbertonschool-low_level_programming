790bd5fafe7d711fb29a5d005126bbb7a04b6319
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-01 17:41:29 +0000

    Array bidimensional

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,32 +14,32 @@ int **alloc_grid(int width, int height)
 	int y;
 	int **p;
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0) /* Si no se recibe nada */
 		return (NULL);
 
-	p = malloc(sizeof(int *) * height);
+	p = malloc(sizeof(int *) * height); /* Creo espacio de memoria para las filas */
 
-	if (p == NULL)
+	if (p == NULL) /* No hay espacio de memoria suficiente */
 	{
 		free(p);
 		return (NULL);
 	}
 
-	for (x = 0; x < height; x++)
+	for (x = 0; x < height; x++) /* Recorro cada una de las filas */
 	{
-		p[x] = malloc(sizeof(int) * width);
+		p[x] = malloc(sizeof(int) * width); /* Creo espacio de memoria para las columnas por fila */
 
-		if (p[x] == NULL)
+		if (p[x] == NULL) /* No hay espacio de memoria suficiente para las columnas */
 		{
-			for (; x >= 0; x--)
+			for (; x >= 0; x--) /*Limpio uno a uno los espacios creados */
 				free(p[x]);
 
-			free(p);
+			free(p); /* Limpio todo el espacio */
 			return (NULL);
 		}
 	}
 
-	for (x = 0; x < height; x++)
+	for (x = 0; x < height; x++) /*Por fila y columna le adjudico un valor a el espacio */
 	{
 		for (y = 0; y < width; y++)
 			p[x][y] = 0;
