97cfb7150ea015d07d4b7d8820438416b192eb2f
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-02 17:02:09 +0000

    Concatenar Strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for  (i = 0; i <= sum; i++)
 	{
-		p[i] = s1[i];
+		if (i < len)
+			p[i] = s1[i];
 
 		if (i >= len && x <= (len2 - 1))
 		{
