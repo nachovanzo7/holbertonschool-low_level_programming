eb27411539f632fd151c64fb707733c9ef1c624d
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-02 14:22:42 +0000

    Reservar espacio en memoria

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - Asignar memoria usando malloc
+ *Return: no retorna nada
+ *@b: espacio de memoria a alojar
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(sizeof(p) * b);
+
+	if (p == NULL)
+		exit(98);
+
+	exit(0);
+}
