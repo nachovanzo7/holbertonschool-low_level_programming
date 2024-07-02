13cd996d7b2d1b5ffca51f4f21c336b3ad29d4c4
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-02 17:47:06 +0000

    Concatenar Strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,46 +11,49 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int sum, len = 0, len2 = 0, i, x = 0;
-	char *p;
+        unsigned int sum, len = 0, len2 = 0, i, x = 0;
+        char *p;
 
-	if (s1 == NULL)
-		s1 = "";
+        if (s1 == NULL)
+                s1 = "";
 
-	if (s2 == NULL)
-		s2 = "";
+        if (s2 == NULL)
+                s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
-		len++;
+        for (i = 0; s1[i] != '\0'; i++)
+                len++;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		len2++;
+        for (i = 0; s2[i] != '\0'; i++)
+                len2++;
 
-	if (len2 > n)
-		sum = (len - 1) + n;
+        if (len2 > n)
+                sum = (len - 1) + n;
 
-	else
-		sum = (len - 1) + len2;
+        else
+                sum = (len - 1) + len2;
 
-	p = malloc(sizeof(char) * (sum));
+        p = malloc(sizeof(char) * (sum));
 
-	if (p == NULL)
-		return (NULL);
+        if (p == NULL)
+                return (NULL);
 
-	for  (i = 0; i <= (sum); i++)
-	{
-		if (i <= len)
-			p[i] = s1[i];
+        for  (i = 0; i <= (sum); i++)
+        {
+                if (i <= len)
+                        p[i] = s1[i];
 
-		if (i >= len && x <= (len2))
-		{
-			p[i] = s2[x];
-			x++;
-		}
+                if (i >= len && x <= (len2))
+                {
+                        p[i] = s2[x];
+                        x++;
+                }
 
-	}
+        }
 
-	p[sum] = '\0';
+        p[sum] = '\0';
+
+        return (p);
+}
 
 	return (p);
 }
