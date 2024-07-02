1c120bc35d3032d1ba5b8f47112b0acb9a1cfdc1
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-02 16:50:54 +0000

    Concatenar Strings

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *string_nconcat - Concatena dos strings
+ *Return: retorna string concatenado
+ *@s1: string 1
+ *@s2: string 2
+ *@n: longitud de string 2
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int sum, len = 0, len2 = 0, i, x = 0;
+	char *p;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		len++;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		len2++;
+
+	if (len2 > n)
+		sum = len + n;
+
+	else
+		sum = len + len2;
+
+	p = malloc(sizeof(char) * sum);
+
+	if (p == NULL)
+		return (NULL);
+
+	for  (i = 0; i <= sum; i++)
+	{
+		p[i] = s1[i];
+
+		if (i >= len && x <= len2)
+		{
+			p[i] = s2[x];
+			x++;
+		}
+
+	}
+
+	return (p);
+}
