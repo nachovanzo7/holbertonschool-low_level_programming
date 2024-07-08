4009e29478afbc91418c91013d3834a12494839f
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-08 13:28:05 +0000

    Definir nuevo tipo de estructura - typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,6 +18,12 @@ struct dog
 	char *owner;
 };
 
+/* "typedef" - Indica la creacion de un alias 
+ * Sintaxis: typedef estructura nuevonombre */
+
+/* Defino un tipo dog_t como "alias" de struct dog */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
