de78ce7deb23f839ca986069a06a41eedf38f055
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-09 14:41:11 +0000

    Imprimir nombre - Puntero a Funciones

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ *print_name - Imprimir un nombre
+ *Return: no retorna nada
+ *@name: nombre a imprimir
+ *@f: puntero a funcion
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	void (*p)(char *);
+
+	p = f;
+
+	(*p)(name);
+	p(name);
+
+	exit (EXIT_SUCCESS);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef POINTER_H
+#define POINTER_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
