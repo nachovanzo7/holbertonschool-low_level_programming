f8c0ed0ed99ae3ba12c6c778974b7daccf05986e
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-10 17:05:15 +0000

    Imprimir lista de numeros separados por un separador

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int i;
-	char s = *separator;
+	char *s = *separator;
 
 	va_list lista;
 
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%i", va_arg(lista, int));
 
 		if (i < ((int)n - 1))
-			printf("%c", s);
+			printf("%s", s);
 	}
 
 	printf("%c" ,'\n');
