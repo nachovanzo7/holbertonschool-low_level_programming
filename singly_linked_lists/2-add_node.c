2651cf7a13d9e600695dca870af6d05e5b4de870
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-29 17:28:19 +0000

    Reemplazar Nodo head - Add Node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+/**
+*add_node - Reemplaza e imprime el head del Linked Lists
+*Return: retorna la direccion de memoria del head
+*@head: espacio de memoria del primer nodo
+*@str: string pasado por parametro (valor (list_t).strx
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	int i;
+	list_t *p;
+
+	i = 0;
+
+	p = (list_t *)malloc(sizeof(list_t)); /* Asigno memoria para la estructura */
+
+	if (head == NULL)
+		return (NULL);
+
+	p->str = strdup(str);
+
+	if (p->str == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+		i++;
+
+	p->len = i;
+	p->next = *head;
+	*head = p;
+
+	return (p);
+
+
+}
