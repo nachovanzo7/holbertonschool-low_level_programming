5c6e3f1b251b5d770bbff96daf9d2f5511f666db
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-29 18:13:58 +0000

    Libera espacio de memoria - Linked List

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *free_list - Libera la memoria de list_t
+ *Return: no retorna nada
+ *@head: puntero a la lista para liberar
+ */
+
+void free_list(list_t *head)
+{
+    list_t *p;
+
+    while (head)
+    {
+        p = head;
+        head = head->next;
+        free(p->str);
+        free(p);
+    }
+}
