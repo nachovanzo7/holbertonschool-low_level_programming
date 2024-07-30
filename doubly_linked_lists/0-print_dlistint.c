25cae4e39db42eefa3d1abfaeecce47e48c34b44
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-30 17:29:49 +0000

    Doubly linked lists - Ex 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ *print-dlistint - Imprime todo lo que contenga la Lista Doble
+ *Return: retorna el entero contenido
+ *@h: puntero a la estructura
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+	i = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+
+}
