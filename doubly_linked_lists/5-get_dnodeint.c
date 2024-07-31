aa3e696ab87ecee384f3a5a102b3dc2f111b821d
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-31 16:49:07 +0000

    Encontrar y devolver nodo especifico - Ex5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+ *Return: retorna nodo de la lista
+ *@head: nodo de lista
+ *@index: posicion del nodo a devolver
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+	}
+
+	return (head);
+}
