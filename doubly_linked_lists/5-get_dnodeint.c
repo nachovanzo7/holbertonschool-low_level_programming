606a0591d65664dd6d02139c282378cb9e3d895e
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-31 16:51:28 +0000

    Encontrar y devolver nodo especifico - Ex5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -18,6 +18,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	for (i = 0; i < index; i++)
 	{
 		head = head->next;
+		if (head == NULL)
+			return (NULL);
 	}
 
 	return (head);
