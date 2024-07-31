01edb754a127b6be4093fcd2a658d5514f161b22
Nacho Vanzo <9426@holbertonstudents.com>
2024-07-31 16:25:58 +0000

    Liberar espacio de memoria - Ex4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *free_dlistint - Liberar espacio de memoria
+ *Return: no retorna nada
+ *@head: lista a la cual liberar de la memoria
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *p;
